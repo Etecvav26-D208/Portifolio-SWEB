3dd9d63a40808566ea81ee46bb3b4536496322d5
marigemea-diva <marianarez.alves@gmail.com>
2026-03-30 10:39:34 -0300

    Add files via upload
    exercicios php

---
+++ MariaEduarda-MarianaR/Fundamentos-PHP/exercicio1/index.php
@@ -0,0 +1,25 @@
+<!--
+Data: 30/03/2026
+Autor: Maria Eduarda Pinto De Oliveira Rodrigues Mariana Rasmussen Rezende Alves 2°D
+Objetivo:
+
+Exercício 1 - Intervalo entre 100 e 200
+Faça um programa que receba um número e diga se este número está no intervalo entre 100 e 200.
+-->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício 1</title>
+</head>
+<body>
+<h1> Intervalo entre 100 e 200.<h1>
+<form action="calcula.php" method="post">
+   <label> Digite um número: </label><br>
+    <input type="number" name="numero" required>
+    <br><br>
+    <button type="submit">Verificar</button>
+</form>
+
+</body>
+</html>

67127b3c6037d746de57672da18a98e092e9fabc
duda <mariaeduardaporodrigues@gmail.com>
2026-03-30 11:08:16 -0300

    Update index.php

--- MariaEduarda-MarianaR/Fundamentos-PHP/ex03/index.php
+++ MariaEduarda-MarianaR/Fundamentos-PHP/ex03/index.php
@@ -1,6 +1,6 @@
 <!--
 Data: 30/03/2026
-Autor: Amanda Neves Oliveira e Ana Lívia Takeyama Romanato - 2°D
+Autor:Maria Eduarda Pinto de Oliveira Rodrigues e Mariana Rasmussen Rezende Alves- 2°D
 Objetivo: Realizar operações matemáticas básicas com base em um operador escolhido pelo usuário.
 
 Exercício 3 - Calculadora Aritmética

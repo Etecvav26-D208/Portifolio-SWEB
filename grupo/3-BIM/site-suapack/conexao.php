f0795a08d731a2b0db5f2ed328749d8ff62d2eb4
Maria Eduarda <mariaeduardaporodrigues@gmail.com>
2026-08-31 18:38:16 -0300

    criando a conexão entre meus arquivos e meu banco de dados

--- grupo/3-BIM/site-suapack/conexao.php
+++ grupo/3-BIM/site-suapack/conexao.php
@@ -1,1 +1,14 @@
+<?php
 
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "suapack";
+
+$conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+if (!$conexao) {
+    die("Erro na conexão com o banco de dados.");
+}
+
+?>

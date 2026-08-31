8bbe60153680af993a7e377c042b72c098d5639c
Maria Eduarda <mariaeduardaporodrigues@gmail.com>
2026-08-31 18:35:37 -0300

    criação do banco de dados do nosso site

--- grupo/3-BIM/site-suapack/banco/suapack.sql
+++ grupo/3-BIM/site-suapack/banco/suapack.sql
@@ -1,1 +1,45 @@
+CREATE DATABASE suapack;
 
+USE suapack;
+
+
+-- TABELA DE CATEGORIAS
+CREATE TABLE categorias (
+    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+);
+
+
+-- TABELA DE PRODUTOS
+CREATE TABLE produtos (
+    id_produto INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT,
+    preco DECIMAL(10,2) NOT NULL,
+    preco_promocional DECIMAL(10,2),
+    imagem VARCHAR(255),
+    estoque INT DEFAULT 0,
+    em_promocao BOOLEAN DEFAULT FALSE,
+    id_categoria INT
+);
+
+
+-- TABELA DE PEDIDOS
+CREATE TABLE pedidos (
+    id_pedido INT AUTO_INCREMENT PRIMARY KEY,
+    nome_cliente VARCHAR(100) NOT NULL,
+    endereco VARCHAR(255) NOT NULL,
+    forma_pagamento VARCHAR(50) NOT NULL,
+    valor_total DECIMAL(10,2) NOT NULL,
+    status_pedido VARCHAR(50) DEFAULT 'Pendente'
+);
+
+
+-- TABELA DE ITENS DO PEDIDO
+CREATE TABLE itens_pedido (
+    id_item INT AUTO_INCREMENT PRIMARY KEY,
+    id_pedido INT NOT NULL,
+    id_produto INT NOT NULL,
+    quantidade INT NOT NULL,
+    preco_unitario DECIMAL(10,2) NOT NULL
+);

d10884720412b8470518d021d5be90e888630fee
Maria Eduarda <mariaeduardaporodrigues@gmail.com>
2026-09-18 20:52:11 -0300

    Implement product deletion in excluir.php]

--- grupo/3-BIM/site-suapack/admin/produtos/excluir.php
+++ grupo/3-BIM/site-suapack/admin/produtos/excluir.php
@@ -1,1 +1,14 @@
+<?php
 
+include("../../conexao.php");
+
+$id = $_GET["id"];
+
+$sql = "DELETE FROM produtos WHERE id_produto = $id";
+
+mysqli_query($conexao, $sql);
+
+header("Location: listar.php");
+exit;
+
+?>

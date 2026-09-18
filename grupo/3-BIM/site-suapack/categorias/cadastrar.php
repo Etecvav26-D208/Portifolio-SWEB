e5e2abb862e2a4f250cc0d42a3baae37ef6b2b47
Maria Eduarda <mariaeduardaporodrigues@gmail.com>
2026-09-18 18:25:49 -0300

    criação do php para cadastrar os produtos

--- grupo/3-BIM/site-suapack/categorias/cadastrar.php
+++ grupo/3-BIM/site-suapack/categorias/cadastrar.php
@@ -1,1 +1,40 @@
+<?php
 
+include("../conexao.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $nome = $_POST["nome"];
+
+    $sql = "INSERT INTO categorias (nome) VALUES ('$nome')";
+
+    mysqli_query($conexao, $sql);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastrar Categoria - SUA PACK</title>
+</head>
+
+<body>
+
+    <h1>Cadastrar Categoria</h1>
+
+    <form method="POST">
+
+        <label>Nome da categoria:</label>
+
+        <input type="text" name="nome" required>
+
+        <button type="submit">Cadastrar</button>
+
+    </form>
+
+</body>
+
+</html>

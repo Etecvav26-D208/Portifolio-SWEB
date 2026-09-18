4d223eba4c933e5aeb321df78befc58e6c9cb90f
Maria Eduarda <mariaeduardaporodrigues@gmail.com>
2026-09-18 20:45:56 -0300

    Implement delete functionality for categories

--- grupo/3-BIM/site-suapack/categorias/excluir.php
+++ grupo/3-BIM/site-suapack/categorias/excluir.php
@@ -1,1 +1,14 @@
+<?php
 
+include("../../conexao.php");
+
+$id = $_GET["id"];
+
+$sql = "DELETE FROM categorias WHERE id_categoria = $id";
+
+mysqli_query($conexao, $sql);
+
+header("Location: listar.php");
+exit;
+
+?>

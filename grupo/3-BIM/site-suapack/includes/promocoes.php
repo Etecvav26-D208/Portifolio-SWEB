dc5a913bdc08155a348a753dbc00bbd7bca8afe2
Maria Eduarda <mariaeduardaporodrigues@gmail.com>
2026-09-19 14:38:25 -0300

    Implement promotional products retrieval
    Add SQL query to fetch promotional products with categories.

--- grupo/3-BIM/site-suapack/includes/promocoes.php
+++ grupo/3-BIM/site-suapack/includes/promocoes.php
@@ -1,1 +1,18 @@
+<?php
 
+include("../conexao.php");
+
+
+/* Busca os produtos que estão em promoção */
+
+$sql = "SELECT produtos.*, categorias.nome AS categoria
+        FROM produtos
+        LEFT JOIN categorias
+        ON produtos.id_categoria = categorias.id_categoria
+        WHERE produtos.em_promocao = 1
+        ORDER BY produtos.id_produto DESC";
+
+
+$resultado_promocoes = mysqli_query($conexao, $sql);
+
+?>

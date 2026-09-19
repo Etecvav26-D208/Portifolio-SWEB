351f5644724f34f9f89470244a2722ab808e52af
Maria Eduarda <mariaeduardaporodrigues@gmail.com>
2026-09-19 14:32:57 -0300

    Implement delete functionality for pedidos

--- grupo/3-BIM/site-suapack/admin/pedidos/excluir.php
+++ grupo/3-BIM/site-suapack/admin/pedidos/excluir.php
@@ -1,1 +1,15 @@
 
+<?php
+
+include("../../conexao.php");
+
+$id = $_GET["id"];
+
+$sql = "DELETE FROM pedidos WHERE id_pedido = $id";
+
+mysqli_query($conexao, $sql);
+
+header("Location: listar.php");
+exit;
+
+?>

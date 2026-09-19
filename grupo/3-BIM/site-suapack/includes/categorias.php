3a35bc2e33d5dcb11203e988f3eb6d23df5b3f16
Maria Eduarda <mariaeduardaporodrigues@gmail.com>
2026-09-19 14:37:49 -0300

    Create categorias.php for category retrieval
    Add categorias.php to fetch categories from the database

---
+++ grupo/3-BIM/site-suapack/includes/categorias.php
@@ -0,0 +1,13 @@
+<?php
+
+include("../conexao.php");
+
+
+/* Busca todas as categorias */
+
+$sql = "SELECT * FROM categorias ORDER BY nome";
+
+
+$resultado_categorias = mysqli_query($conexao, $sql);
+
+?>

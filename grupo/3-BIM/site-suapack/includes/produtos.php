6dffa656818da7af988c9cb0fc4ba619a3ab2840
Maria Eduarda <mariaeduardaporodrigues@gmail.com>
2026-09-19 14:36:57 -0300

    Implement product retrieval with category join
    Added SQL query to fetch products with categories.

--- grupo/3-BIM/site-suapack/includes/produtos.php
+++ grupo/3-BIM/site-suapack/includes/produtos.php
@@ -1,1 +1,17 @@
+<?php
 
+include("../conexao.php");
+
+
+/* Busca todos os produtos */
+
+$sql = "SELECT produtos.*, categorias.nome AS categoria
+        FROM produtos
+        LEFT JOIN categorias
+        ON produtos.id_categoria = categorias.id_categoria
+        ORDER BY produtos.id_produto DESC";
+
+
+$resultado_produtos = mysqli_query($conexao, $sql);
+
+?>

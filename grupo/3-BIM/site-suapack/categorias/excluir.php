88225e4d4ac6af89eabca292fa171d356a302673
Maria Eduarda <mariaeduardaporodrigues@gmail.com>
2026-09-20 18:24:14 -0300

    Remove category by ID in excluir.php

--- grupo/3-BIM/site-suapack/categorias/excluir.php
+++ grupo/3-BIM/site-suapack/categorias/excluir.php
@@ -4,11 +4,18 @@ include("../../conexao.php");
 
 $id = $_GET["id"];
 
+
+/* Exclui a categoria */
+
 $sql = "DELETE FROM categorias WHERE id_categoria = $id";
 
 mysqli_query($conexao, $sql);
 
+
+/* Volta para a lista */
+
 header("Location: listar.php");
+
 exit;
 
 ?>

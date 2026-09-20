29a29823fbc264cdbda50323b4f544c0101b76f5
Maria Eduarda <mariaeduardaporodrigues@gmail.com>
2026-09-20 18:19:43 -0300

    Add product deletion functionality in excluir.php

--- grupo/3-BIM/site-suapack/admin/produtos/excluir.php
+++ grupo/3-BIM/site-suapack/admin/produtos/excluir.php
@@ -2,13 +2,23 @@
 
 include("../../conexao.php");
 
+
+/* Pega o ID do produto */
+
 $id = $_GET["id"];
+
+
+/* Exclui o produto */
 
 $sql = "DELETE FROM produtos WHERE id_produto = $id";
 
 mysqli_query($conexao, $sql);
 
+
+/* Volta para a lista */
+
 header("Location: listar.php");
+
 exit;
 
 ?>
